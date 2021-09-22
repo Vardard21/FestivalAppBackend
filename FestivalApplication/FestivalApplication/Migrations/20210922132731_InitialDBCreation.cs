b539aa7eb5a574840177403dbf54950c4380dd16
rubengass <ruben.gass@ciber.nl>
2021-09-22 16:03:02 +0200

    label

---
+++ FestivalApplication/FestivalApplication/Migrations/20210922132731_InitialDBCreation.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace FestivalApplication.Migrations
+{
+    public partial class InitialDBCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    Role = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserActivity",
+                columns: table => new
+                {
+                    UserActivityID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    StageID = table.Column<int>(type: "int", nullable: false),
+                    Entry = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Exit = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserActivity", x => x.UserActivityID);
+                    table.ForeignKey(
+                        name: "FK_UserActivity_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    MessageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    MessageText = table.Column<string>(type: "text", nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime", nullable: false),
+                    UserActivityID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.MessageID);
+                    table.ForeignKey(
+                        name: "FK_Message_UserActivity_UserActivityID",
+                        column: x => x.UserActivityID,
+                        principalTable: "UserActivity",
+                        principalColumn: "UserActivityID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Message_UserActivityID",
+                table: "Message",
+                column: "UserActivityID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivity_UserID",
+                table: "UserActivity",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+
+            migrationBuilder.DropTable(
+                name: "UserActivity");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
