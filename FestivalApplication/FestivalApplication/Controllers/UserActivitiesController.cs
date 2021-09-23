b7a25eb2c01d5cdbf47df077d806228e644bb966
rubengass <ruben.gass@ciber.nl>
2021-09-23 14:39:44 +0200

    label

---
+++ FestivalApplication/FestivalApplication/Controllers/UserActivitiesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FestivalApplication.Data;
+using FestivalApplication.Model;
+
+namespace FestivalApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserActivitiesController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public UserActivitiesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        // PUT: api/UserActivities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserActivity(int id, UserActivity userActivity)
+        {
+            if (id != userActivity.UserActivityID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userActivity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserActivityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ FestivalApplication/FestivalApplication/Model/DataTransferObjects/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FestivalApplication.Model.DataTransferObjects
+{
+    public class Response<o>
+    {
+        public Boolean Success { get; set; }
+        public List<int> ErrorMessage { get; set; } = new List<int>();
+        public o Data { get; set; }
+    }
+}
