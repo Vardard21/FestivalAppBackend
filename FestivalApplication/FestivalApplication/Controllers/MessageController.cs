3c75c5b8361dc255634a775a7f3668a8946ebfea
rubengass <ruben.gass@ciber.nl>
2021-09-27 13:08:49 +0200

    Merge Ruben 27-09

--- FestivalApplication/FestivalApplication/Controllers/MessageController.cs
+++ FestivalApplication/FestivalApplication/Controllers/MessageController.cs
@@ -11,7 +11,7 @@ using FestivalApplication.Model.DataTransferObjects;
 
 namespace FestivalApplication.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Messages")]
     [ApiController]
     public class MessageController : ControllerBase
     {
@@ -27,39 +27,49 @@ namespace FestivalApplication.Controllers
         public Response<List<MessageSendDto>> GetMessage(ChatHistoryDto historyrequest)
         {
             Response<List<MessageSendDto>> response = new Response<List<MessageSendDto>>();
+
             try
             {
-                //Validate LastUpdated date and set default to one hour ago
-                //if (historyrequest.LastUpdated < DateTime.UtcNow.AddHours(-1))
-                //{
-                //    historyrequest.LastUpdated = DateTime.UtcNow.AddHours(-1);
-                //}
-
-                //Check for all messages posted in the stage since the last update date
-                var MessageHistory = _context.Message.Where(x => x.Timestamp > historyrequest.LastUpdated && x.UserActivity.StageID == historyrequest.StageID).Include(message => message.UserActivity).ToList();
-
-                //Initialize the return list
-                List<MessageSendDto> ChatHistory = new List<MessageSendDto>();
-
-                //Convert Message objects to MessageSendDto objects and populate the return list
-                foreach (Message message in MessageHistory)
+                AuthenticateKey auth = new AuthenticateKey();
+                if (!auth.Authenticate(_context, Request.Headers["Authorization"]))
                 {
-                    //Create a new Send Message Object and fill the text and timestamp
-                    MessageSendDto dto = new MessageSendDto();
-                    dto.MessageText = message.MessageText;
-                    dto.Timestamp = message.Timestamp;
-                    //Find the author by UserID and assign the UserName and UserRole
-                    User Author = _context.User.Find(message.UserActivity.UserID);
-                    dto.UserName = Author.UserName;
-                    dto.UserRole = Author.Role;
-                    //Add the new object to the return list
-                    ChatHistory.Add(dto);
-                }
+                    //Validate LastUpdated date and set default to one hour ago
+                    if (historyrequest.LastUpdated < DateTime.UtcNow.AddHours(-1))
+                    {
+                        historyrequest.LastUpdated = DateTime.UtcNow.AddHours(-1);
+                    }
 
-                //Return the Dto list
-                response.Success = true;
-                response.Data = ChatHistory;
-                return response;
+                    //Check for all messages posted in the stage since the last update date
+                    var MessageHistory = _context.Message.Where(x => x.Timestamp > historyrequest.LastUpdated && x.UserActivity.StageID == historyrequest.StageID).Include(message => message.UserActivity).ToList();
+
+                    //Initialize the return list
+                    List<MessageSendDto> ChatHistory = new List<MessageSendDto>();
+
+                    //Convert Message objects to MessageSendDto objects and populate the return list
+                    foreach (Message message in MessageHistory)
+                    {
+                        //Create a new Send Message Object and fill the text and timestamp
+                        MessageSendDto dto = new MessageSendDto();
+                        dto.MessageText = message.MessageText;
+                        dto.Timestamp = message.Timestamp;
+                        //Find the author by UserID and assign the UserName and UserRole
+                        User Author = _context.User.Find(message.UserActivity.UserID);
+                        dto.UserName = Author.UserName;
+                        dto.UserRole = Author.Role;
+                        //Add the new object to the return list
+                        ChatHistory.Add(dto);
+                    }
+
+                    //Return the Dto list
+                    response.Success = true;
+                    response.Data = ChatHistory;
+                    return response;
+                } else
+                {
+                    response.Success = false;
+                    response.ErrorMessage.Add(5);
+                    return response;
+                }
             }
             catch
             {
@@ -67,11 +77,9 @@ namespace FestivalApplication.Controllers
                 response.ErrorMessage.Add(1);
                 return response;
             }
-
         }
 
         // POST: api/Message
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public Response<string> PostMessage(MessageReceiveDto messagedto)
         {
@@ -79,38 +87,50 @@ namespace FestivalApplication.Controllers
             Response<string> response = new Response<string>();
 
             try {
-                //Create a new message to transfer the message data into
-                Message message = new Message();
-                //Find UserActivities currently active for the UserID
-                var activitiesfound = _context.UserActivity.Where(x => x.UserID == messagedto.UserID && x.Exit == default).ToList();
-
-                //Check if the user is currently active in an activity
-                if (activitiesfound.Count() == 1)
+                AuthenticateKey auth = new AuthenticateKey();
+                if (!auth.Authenticate(_context, Request.Headers["Authorization"]))
                 {
-                    //Insert message text, UserActivity and timestamp into message object
-                    message.UserActivity = activitiesfound[0];
-                    message.MessageText = messagedto.MessageText;
-                    message.Timestamp = DateTime.UtcNow;
+                    //Create a new message to transfer the message data into
+                    Message message = new Message();
+                    //Find UserActivities currently active for the UserID
+                    var activitiesfound = _context.UserActivity.Where(x => x.UserID == messagedto.UserID && x.Exit == default).ToList();
 
-                    //Save the message object
-                    _context.Message.Add(message);
-                    if (_context.SaveChanges() > 0)
+                    //Check if the user is currently active in an activity
+                    if (activitiesfound.Count() == 1)
                     {
-                        //Message was saved correctly
-                        response.Success = true;
-                        return response;
-                    } else
+                        //Insert message text, UserActivity and timestamp into message object
+                        message.UserActivity = activitiesfound[0];
+                        message.MessageText = messagedto.MessageText;
+                        message.Timestamp = DateTime.UtcNow;
+
+                        //Save the message object
+                        _context.Message.Add(message);
+                        if (_context.SaveChanges() > 0)
+                        {
+                            //Message was saved correctly
+                            response.Success = true;
+                            return response;
+                        }
+                        else
+                        {
+                            //Message was not saved correctly
+                            response.Success = false;
+                            response.ErrorMessage.Add(1);
+                            return response;
+                        }
+                    }
+                    else
                     {
-                        //Message was not saved correctly
+                        //There was no active UserActivity for this user
                         response.Success = false;
-                        response.ErrorMessage.Add(1);
+                        response.ErrorMessage.Add(3);
                         return response;
                     }
-                } else
+                }
+                else
                 {
-                    //There was no active UserActivity for this user
                     response.Success = false;
-                    response.ErrorMessage.Add(3);
+                    response.ErrorMessage.Add(5);
                     return response;
                 }
             }
---
+++ FestivalApplication/FestivalApplication/Model/AuthenticateKey.cs
@@ -0,0 +1,37 @@
+﻿using FestivalApplication.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FestivalApplication.Model
+{
+    public class AuthenticateKey
+    {
+        public Boolean Authenticate(DBContext context, string Key)
+        {
+            context.Authentication.RemoveRange(context.Authentication.Where(x => x.CurrentExpiryDate > DateTime.UtcNow));
+            if (context.Authentication.Any(x => x.AuthenticationKey == Key))
+            {
+                Authentication auth = context.Authentication.Where(x => x.AuthenticationKey == Key).FirstOrDefault();
+                if (auth.MaxExpiryDate < DateTime.UtcNow.AddMinutes(15))
+                {
+                    auth.CurrentExpiryDate = auth.MaxExpiryDate;
+                }
+                else
+                {
+                    auth.CurrentExpiryDate = DateTime.UtcNow.AddMinutes(15);
+                }
+                context.Entry(auth).State = EntityState.Modified;
+                context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
