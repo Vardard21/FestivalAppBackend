b973fa533af24d30db49c0b03b399c706dcf035f
rubengass <ruben.gass@ciber.nl>
2021-09-27 13:15:36 +0200

    Merge Ruben 27-09

--- FestivalApplication/FestivalApplication/Model/DataTransferObjects/Response.cs
+++ FestivalApplication/FestivalApplication/Model/DataTransferObjects/Response.cs
@@ -10,5 +10,31 @@ namespace FestivalApplication.Model.DataTransferObjects
         public Boolean Success { get; set; }
         public List<int> ErrorMessage { get; set; } = new List<int>();
         public o Data { get; set; }
+
+        public void ServerError()
+        {
+            Success = false;
+            ErrorMessage.Add(1);
+        }
+        public void RequestError()
+        {
+            Success = false;
+            ErrorMessage.Add(2);
+        }
+        public void InvalidOperation()
+        {
+            Success = false;
+            ErrorMessage.Add(2);
+        }
+        public void InvalidData()
+        {
+            Success = false;
+            ErrorMessage.Add(4);
+        }
+        public void AuthorizationError()
+        {
+            Success = false;
+            ErrorMessage.Add(5);
+        }
     }
 }
