4f2862ddc77adab09488bcaf10e1abf37e4daae2
Ruben Gass <ruben.gass@ciber.nl>
2021-10-01 11:17:08 +0200

    label

--- FestivalApplication/FestivalApplication/Model/DataTransferObjects/StageCreateDto.cs
+++ FestivalApplication/FestivalApplication/Model/DataTransferObjects/StageCreateDto.cs
@@ -7,6 +7,7 @@ namespace FestivalApplication.Model.DataTransferObjects
 {
     public class StageCreateDto
     {
+        public int StageID { get; set; }
         public string StageName { get; set; }
         public bool StageActive { get; set; }
     }
